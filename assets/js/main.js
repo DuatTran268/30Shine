fcf0705bfae99be918bd9af62e8521bd4638c30c
DuatTran268 <duatsen36@gmail.com>
2022-04-07 19:57:11 +0700

    layout services

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,78 @@ const swiper = new Swiper(".home-swiper", {
 
 // slick slider
 // JavaScript Document
-$(document).ready(function () {
-  $("#autoWidth").lightSlider({
-    autoWidth: true,
-    loop: true,
-    onSliderLoad: function () {
-      $("#autoWidth").removeClass("cS-hidden");
+
+$(".slider").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2, // số slide muốn hiển thị
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
     },
-  });
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+
+// slick slider
+// JavaScript Document
+$(".slide2").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3.5, // số slide muốn hiển thị
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
 // sticky slider
