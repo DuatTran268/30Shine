c784d8807f757827386eb81478a03eada4298900
DuatTran268 <duatsen36@gmail.com>
2022-04-08 00:21:17 +0700

    update 5 service

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// slide banner
 const swiper = new Swiper(".home-swiper", {
   autoplay: {
     delay: 3000,
@@ -14,9 +15,7 @@ const swiper = new Swiper(".home-swiper", {
   },
 });
 
-// slick slider
-// JavaScript Document
-
+// slick slider hành trình tỏa sáng
 $(".slider").slick({
   dots: true,
   infinite: false,
@@ -53,14 +52,13 @@ $(".slider").slick({
   ],
 });
 
-// slick slider
-// JavaScript Document
+// slick slider2 Angels girl
 $(".slide2").slick({
   dots: true,
   infinite: false,
   speed: 300,
   slidesToShow: 3.5, // số slide muốn hiển thị
-  slidesToScroll: 1,
+  slidesToScroll: 2,
   responsive: [
     {
       breakpoint: 1024,
@@ -90,4 +88,37 @@ $(".slide2").slick({
     // instead of a settings object
   ],
 });
-// sticky slider
+
+// sticky slider3 Shine moment
+$(".slide3").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4, // số slide muốn hiển thị
+  slidesToScroll: 1, // số lần break của silde
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
